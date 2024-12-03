d7d4c55c4913df452be14d53ccc1e879494d2e2b
Patrick van der Linden <p.vanderlinden@medscan.nl>
2024-12-03 03:53:50 +0100

    Add Part B of the Day 2 puzzle

--- AdventOfCode2024/Puzzles/Puzzle2.cs
+++ AdventOfCode2024/Puzzles/Puzzle2.cs
@@ -57,9 +57,11 @@ public class Puzzle2 : IPuzzle
             throw new InvalidOperationException("The input should be parsed first.");
         }
 
-        // Puzzle 2 asks to give the total sum of reports that are considered 'safe'. For the report to be safe, each
+        // Puzzle 2 asks to give the total sum of reports that are considered 'Safe'. For the report to be Safe, each
         // report is checked to see if all of its containing levels are all increasing or all decreasing. Any two
-        // adjacent levels must also differ by at least one and at most three.
+        // adjacent levels must also differ by at least one and at most three. Part B of the puzzle adds one exception
+        // to this rule. That is, that one, and only one, level in the sequence can be ignored for the report to be
+        // considered Safe.
         var safeReportsCount = 0;
 
         Console.ForegroundColor = ConsoleColor.Gray;
@@ -93,53 +95,77 @@ public class Puzzle2 : IPuzzle
 
     private bool IsReportSafe(Report report)
     {
-        var lastLevel = -1;
-        var wasIncreasing = false;
-        var wasDecreasing = false;
-
-        foreach (var level in report.Levels)
+        // If the report contains only one level or less, it can already be marked as Safe as there is no sequence to check.
+        if (report.Levels.Count <= 1)
         {
-            if (lastLevel == -1)
-            {
-                lastLevel = level;
-                continue;
-            }
-
-            if (lastLevel == level)
-            {
-                // Unsafe. The current level has the same value as the previous level, but the levels must change by at
-                // least 1.
-                return false;
-            }
-
-            var isIncreasing = level > lastLevel;
-            var isDecreasing = level < lastLevel;
-
-            if (isDecreasing && wasIncreasing)
-            {
-                // Unsafe. The current level is decreasing, while the previous level was increasing.
-                return false;
-            }
-
-            if (isIncreasing && wasDecreasing)
-            {
-                // Unsafe. The current level is increasing, while the previous level was decreasing.
-                return false;
-            }
-
-            if ((isIncreasing || isDecreasing) && Math.Abs(lastLevel - level) > 3)
-            {
-                // Unsafe. The current level differs by more than 3 compared to the previous level.
-                return false;
-            }
-
-            lastLevel = level;
-            wasIncreasing = isIncreasing;
-            wasDecreasing = isDecreasing;
+            return true;
         }
 
-        // When the code reaches this point, all fail-safe checks have been passed.
-        return true;
+        // To make sure all edge-case scenarios are caught, it's needed to compare and track the level differences in
+        // both sequence directions (from start to end, and from end to start). Only a single iteration is needed for this.
+        var safeListStartToEnd = new List<int> { report.Levels[0] };
+        var safeListEndToStart = new List<int> { report.Levels[^1] };
+
+        var directionSafeListStartToEnd = 0;
+        var directionSafeListEndToStart = 0;
+
+        var startToEndIndex = 1;
+        var endToStartIndex = report.Levels.Count - 2;
+
+        while (endToStartIndex >= 0)
+        {
+            var levelA = report.Levels[startToEndIndex];
+            var levelB = report.Levels[endToStartIndex];
+
+            var previousLevelA = safeListStartToEnd[^1];
+            var previousLevelB = safeListEndToStart[^1];
+
+            var directionLevelA = GetLevelDirection(previousLevelA, levelA);
+            var directionLevelB = GetLevelDirection(previousLevelB, levelB);
+
+            // Check if the next level in the start-to-end sequence is Safe.
+            if (IsLevelDifferenceSafe(previousLevelA, levelA)
+                && (safeListStartToEnd.Count == 1 || directionLevelA == directionSafeListStartToEnd))
+            {
+                safeListStartToEnd.Add(levelA);
+                directionSafeListStartToEnd = directionLevelA;
+            }
+
+            // Check if the next level in the end-to-start sequence is Safe.
+            if (IsLevelDifferenceSafe(previousLevelB, levelB)
+                && (safeListEndToStart.Count <= 1 || directionLevelB == directionSafeListEndToStart))
+            {
+                safeListEndToStart.Add(levelB);
+                directionSafeListEndToStart = directionLevelB;
+            }
+
+            startToEndIndex += 1;
+            endToStartIndex -= 1;
+        }
+
+        // The report is considered Safe if any of the two tracked directions list count is equal to or higher than the
+        // total report levels count minus 1.
+        var isSafe = safeListStartToEnd.Count >= report.Levels.Count - 1
+               || safeListEndToStart.Count >= report.Levels.Count - 1;
+        return isSafe;
+    }
+
+    private int GetLevelDirection(int left, int right)
+    {
+        var difference = right - left;
+        var direction = difference switch
+        {
+            > 0 => 1,
+            0 => 0,
+            _ => -1
+        };
+        return direction;
+    }
+
+    private bool IsLevelDifferenceSafe(int left, int right)
+    {
+        var difference = Math.Abs(left - right);
+        return difference is >= 1 and <= 3;
     }
 
     private record Report(List<int> Levels);
